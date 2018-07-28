4f31c11be5aba3e9bd12fca0010e1b362c2f626b
ipshita712 <ipshita.712@gmail.com>
2018-07-28 17:14:13 +0530

    Adding changes to test continuos polling

--- src/main/java/hello/HelloWorld.java
+++ src/main/java/hello/HelloWorld.java
@@ -5,7 +5,8 @@ import org.joda.time.LocalTime;
 public class HelloWorld {
     public static void main(String[] args) {
       LocalTime currentTime = new LocalTime();
-		  System.out.println("The current time is: " + currentTime);	  
+		  System.out.println("The current time is: " + currentTime);
+		  System.out.println("Testing continuous polling");
 
         Greeter greeter = new Greeter();
         System.out.println(greeter.sayHello());
